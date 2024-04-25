e6bb1bf52d37dcbc69ab0c56bde5543330903d48
Gavin Smithson <123982599+Gavin-Smithson@users.noreply.github.com>
2024-04-25 00:08:44 -0400

    Update firebaseConfig.js

--- Final/IDIT/firebaseConfig.js
+++ Final/IDIT/firebaseConfig.js
@@ -9,14 +9,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDwsRsBiF8ua5PqSKXNHE7WweX2WQfpzzE",
-  authDomain: "fireiditproject.firebaseapp.com",
-  databaseURL: "https://fireiditproject-default-rtdb.firebaseio.com",
-  projectId: "fireiditproject",
-  storageBucket: "fireiditproject.appspot.com",
-  messagingSenderId: "337409340652",
-  appId: "1:337409340652:web:0ce661e1dd156953a621c9",
-  measurementId: "G-LVZ36BSF4B"
+  // Use your own keys
 };
 
 // Initialize Firebase
